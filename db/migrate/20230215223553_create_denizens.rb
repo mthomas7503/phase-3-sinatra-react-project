8838b5f25a6ab168bb1705e76e19224d4169659e
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2023-02-15 17:41:52 -0500

    create town model

--- db/migrate/20230215223553_create_denizens.rb
+++ db/migrate/20230215223553_create_denizens.rb
@@ -1,4 +1,10 @@
 class CreateDenizens < ActiveRecord::Migration[6.1]
   def change
+    create_table :denizens do |t|
+      t.string :name
+      t.integer :age
+      t.string :description
+      t.integer :town_id
+    end
   end
 end
