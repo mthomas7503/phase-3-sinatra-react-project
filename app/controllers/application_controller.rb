08b4bac66dc5d4243886fef017ca691744cbd1c4
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2023-02-19 14:49:36 -0500

    create zone and monster route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,11 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/zones" do
+    zones = Zone.all
+    zones.to_json
   end
 
+  get "/monsters" do
+    monsters = Monster.all
 end
